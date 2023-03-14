3aa4d7bf04de65ae32d39697e8289eee834a2773
AlexDD2003 <115020510+AlexDD2003@users.noreply.github.com>
2023-03-14 19:26:12 +0100

    V3

--- database/migrations/2023_03_14_163845_create_users_table.php
+++ database/migrations/2023_03_14_163845_create_users_table.php
@@ -13,11 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->unsignedBigInteger('rol_id');
+            $table->foreign('rol_id')->references('id')->on('roles');
+            $table->timestamps();
         });
+
     }
 
     /**
@@ -27,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('users');
     }
 };
